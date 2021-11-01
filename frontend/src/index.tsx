45bc50bd59fee6bd91840681fa3ca0398ea0a64b
Stephanie-Castro <dev.stephanie.castro@gmail.com>
2021-11-01 11:04:17 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'; //bootstrap
+/*
+  Não é necessário colocar ../src/ porque no arquivo tsconfig.json
+  foi colocado "baseUrl": "./src"
+*/
+//import "../src/assets/css/styles.css"; //estilos criados
+import "assets/css/styles.css"; //estilos criados
 import App from './App';
 
 ReactDOM.render(
