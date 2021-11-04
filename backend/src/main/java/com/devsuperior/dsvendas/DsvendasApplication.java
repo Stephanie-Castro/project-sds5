cd52171520836e1e69408d6077dc0d77c57812d1
Stephanie-Castro <dev.stephanie.castro@gmail.com>
2021-11-04 09:57:43 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -2,8 +2,12 @@ package com.devsuperior.dsvendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
+@ComponentScan({"config"})
 public class DsvendasApplication {
 
 	public static void main(String[] args) {
