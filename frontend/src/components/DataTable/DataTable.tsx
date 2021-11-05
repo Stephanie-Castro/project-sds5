65718250c3914930af81df04ab36481cb5fee90f
Stephanie-Castro <dev.stephanie.castro@gmail.com>
2021-11-05 13:33:54 -0300

    Pagination

--- frontend/src/components/DataTable/DataTable.tsx
+++ frontend/src/components/DataTable/DataTable.tsx
@@ -1,3 +1,4 @@
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
@@ -12,37 +13,46 @@ const DataTable = () => {
     last: true,
   });
 
+  const[activePage, setActivePage] = useState(0);
+
   useEffect(() => {
-    getAllSalesWithPagination(0, 10).then((data) => {
+    getAllSalesWithPagination(activePage, 20).then((data) => {
       setPage(data);
     });
-  }, []);
+  }, [activePage]);
+
+  function changePage(index: number) {
+    setActivePage(index);
+  }
 
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {page.content?.map((item, index) => (
-            <tr key={index}>
-              <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
-              <td>{item.seller.name}</td>
-              <td>{item.visited}</td>
-              <td>{item.deals}</td>
-              <td>{item.amount.toFixed(2)}</td>
+    <>
+      <Pagination page={page} onPageChange={changePage} />
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {page.content?.map((item, index) => (
+              <tr key={index}>
+                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                <td>{item.seller.name}</td>
+                <td>{item.visited}</td>
+                <td>{item.deals}</td>
+                <td>{item.amount.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
